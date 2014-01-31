9a57ad51bbd164185210ea5416539efcfe55f56d
Yonah Forst <joshblour@hotmail.com>
2014-01-31 16:30:20 +0100

    removed checkout steps

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,10 +1,3 @@
 Spree::Order.class_eval do
-  #replace :delivery to any other state 
-  checkout_flow do
-    go_to_state :address
-    go_to_state :payment
-    go_to_state :complete
-  end  
-  
   remove_checkout_step :delivery
 end
